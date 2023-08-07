4083aab3c1e34822e2e7bb421d1317b9d434c451
Jonathan Rehem <jonathan.rehem@outlook.com>
2023-08-07 18:21:38 -0300

    refactor(user): refactor user (#2)

--- packages/shared/src/domain/user/entities/User.ts
+++ packages/shared/src/domain/user/entities/User.ts
@@ -1,38 +1,20 @@
 import { BaseEntity } from "@/domain/core/entities/BaseEntity";
 import { Guid } from "@/domain/core/value-objects/Guid";
 import { Email } from "@/domain/user/valueObjects/Email";
-import { Name } from "@/domain/user/valueObjects/Name";
 import { Password } from "@/domain/user/valueObjects/Password";
 
-type AccountType = "main" | "sub";
-
 export interface UserProps {
-  name: Name;
   email: Email;
   password: Password;
-  accountType: AccountType;
-  mainAccountId?: Guid | null;
-  subAccounts?: User[];
 }
 
 export class User extends BaseEntity<UserProps> {
   constructor(props: UserProps, id?: Guid) {
     super(props, id);
     this.addNotifications([
-      ...this._props.name.notifications,
       ...this._props.email.notifications,
       ...this._props.password.notifications,
     ]);
-  }
-
-  get name(): Name {
-    return this._props.name;
-  }
-
-  public set name(value: Partial<Name>) {
-    this._props.name.firstName = value.firstName || this._props.name.firstName;
-    this._props.name.lastName = value.lastName || this._props.name.lastName;
-    this.touch();
   }
 
   get email(): Email {
@@ -51,29 +33,5 @@ export class User extends BaseEntity<UserProps> {
   public set password(value: Password) {
     this._props.password = value;
     this.touch();
-  }
-
-  public get accountType(): AccountType {
-    return this._props.accountType;
-  }
-
-  public set accountType(value: AccountType) {
-    this._props.accountType = value;
-  }
-
-  public get mainAccountId(): Guid | null {
-    return this._props.mainAccountId ?? null;
-  }
-
-  private set mainAccountId(value: Guid | null) {
-    this._props.mainAccountId = value;
-  }
-
-  public get subAccounts(): User[] {
-    return this._props.subAccounts ?? [];
-  }
-
-  public set subAccounts(value: User[]) {
-    this._props.subAccounts = value;
   }
 }
--- packages/shared/src/tests/factories/userFactory.ts
+++ packages/shared/src/tests/factories/userFactory.ts
@@ -1,7 +1,6 @@
 import { Guid } from "@/domain/core/value-objects/Guid";
 import { User, type UserProps } from "@/domain/user/entities/User";
 import { Email } from "@/domain/user/valueObjects/Email";
-import { Name } from "@/domain/user/valueObjects/Name";
 import { Password } from "@/domain/user/valueObjects/Password";
 import { faker } from "@faker-js/faker";
 
@@ -9,19 +8,12 @@ export class UserFactory {
   static create(propsToOverride: Partial<UserProps> = {}, id?: Guid): User {
     const user = new User(
       {
-        name: new Name({
-          firstName: faker.person.firstName(),
-          lastName: faker.person.lastName(),
-        }),
         email: new Email({
           address: faker.internet.email(),
         }),
         password: new Password({
           value: faker.internet.password(),
         }),
-        accountType: "main",
-        mainAccountId: null,
-        subAccounts: [],
         ...propsToOverride,
       },
       id,
