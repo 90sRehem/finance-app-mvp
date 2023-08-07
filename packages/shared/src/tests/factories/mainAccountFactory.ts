3a4db2f02e43b3b79d6102c110cb951c9b1483b4
Jonathan Rehem <jonathan.rehem@outlook.com>
2023-08-07 18:55:33 -0300

    Task/tar 24 (#3)
    * refactor(user): refactor user (#2)
    
    * feat(main-account): add main-account entity

---
+++ packages/shared/src/tests/factories/mainAccountFactory.ts
@@ -0,0 +1,36 @@
+import { Guid } from "@/domain/core/value-objects/Guid";
+import {
+  MainAccount,
+  type MainAccountProps,
+} from "@/domain/user/entities/MainAccount";
+import { Email } from "@/domain/user/valueObjects/Email";
+import { Name } from "@/domain/user/valueObjects/Name";
+import { Password } from "@/domain/user/valueObjects/Password";
+import { faker } from "@faker-js/faker";
+
+export class MainAccountFactory {
+  static create(
+    propsToOverride: Partial<MainAccountProps> = {},
+    id?: Guid,
+  ): MainAccount {
+    const mainAccount = new MainAccount(
+      {
+        email: new Email({
+          address: faker.internet.email(),
+        }),
+        password: new Password({
+          value: faker.internet.password(),
+        }),
+        name: new Name({
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+        }),
+        userId: propsToOverride.userId || Guid.create(),
+        ...propsToOverride,
+      },
+      id,
+    );
+
+    return mainAccount;
+  }
+}
